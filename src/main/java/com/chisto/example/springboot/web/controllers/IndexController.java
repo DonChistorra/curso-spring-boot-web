aa625b4954738f28e19812f9fba1ca434e9265de
Don Chistorra <shadowshaoran@gmail.com>
2022-05-08 00:19:38 +0200

    Añadido modelo a método index()

--- src/main/java/com/chisto/example/springboot/web/controllers/IndexController.java
+++ src/main/java/com/chisto/example/springboot/web/controllers/IndexController.java
@@ -1,14 +1,18 @@
 package com.chisto.example.springboot.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/web")
 public class IndexController {
 
-	@RequestMapping(value = {"/index", "/", "/home"}, method = RequestMethod.GET)
-	public String index() {
+	@RequestMapping(value = {"", "/", "/home", "/index"}, method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("titulo", "Titulo configurado por Spring");
+		model.addAttribute("mensaje_bienvenida", "Welcome Springer!");
 		String result = "index";
 		return result;
 	}
