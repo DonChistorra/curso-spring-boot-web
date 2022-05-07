cb78a7f3411000162ecfad36ebd1863e4325da95
Don Chistorra <shadowshaoran@gmail.com>
2022-05-07 17:08:10 +0200

    Commit Inicial

---
+++ src/main/java/com/chisto/example/springboot/web/CursoSpringSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.chisto.example.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringSpringBootWebApplication.class, args);
+	}
+
+}
